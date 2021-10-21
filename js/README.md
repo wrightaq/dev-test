632b05b55498335b29f7db67bb2cf5d1c108a71d
Rob Whittemore <rob@test.com>
2021-10-21 12:41:44 -0400

    Removed unneeded repos

---
+++ js/README.md
@@ -0,0 +1,33 @@
+# Gilded Rose
+
+This is the Gilded Rose kata in JavaScript
+
+## Getting started
+
+Install dependencies
+
+```sh
+npm install
+```
+
+## Running program
+
+Run default:
+
+```sh
+npm start
+```
+
+To run with a certain number of days, simply add a number to the end of the command.
+
+For example, to run 5 days:
+
+```sh
+npm start 5
+```
+
+And to run 25 days:
+
+```sh
+npm start 25
+```
---
+++ js/run_store.js
@@ -0,0 +1,23 @@
+const { Shop, Item } = require("./src/gilded_rose");
+
+const items = [
+  new Item("Bananas", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Eggs", 5, 7),
+  new Item("Eggs", 12, 5),
+  new Item("Canned Beans", 0, 80),
+  new Item("Canned Beans", -1, 80),
+
+  // This Baked good does not work properly yet!
+  new Item("Baked Sourdough Bread", 3, 6),
+];
+
+const days = Number(process.argv[2]) || 2;
+const gildedRose = new Shop(items);
+
+for (let day = 0; day < days; day++) {
+  console.log(`\n-------- day ${day} --------`);
+  console.log("name, sellIn, quality");
+  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+  gildedRose.updateQuality();
+}
