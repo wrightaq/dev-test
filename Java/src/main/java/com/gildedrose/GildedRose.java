4cc452af4d8f4d4a66c6e94e7e31358c3e59b277
Rob Whittemore <rob@test.com>
2022-04-12 11:28:21 -0400

    Added Java

---
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,43 @@
+package com.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals("Aged Brie")) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals("Canned Beans")) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
+
+            if (!items[i].name.equals("Canned Beans")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals("Aged Brie")) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Canned Beans")) {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
