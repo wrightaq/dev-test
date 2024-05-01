77dfd9bfcd185729c572cb81e8a90e985576419c
Kyle McCray <kyle_mccray@shi.com>
2024-05-01 11:22:30 -0600

    updated java code to match csharp net60core

--- Java/src/main/java/com/shimart/ShiMart.java
+++ Java/src/main/java/com/shimart/ShiMart.java
@@ -9,32 +9,36 @@ class ShiMart {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")) {
+            if (!items[i].name.equals("Canned Beans")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].name.equals("Aged Brie")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+
+            } else {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Canned Beans")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
             }
 
-            if (!items[i].name.equals("Canned Beans")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
+                if (items[i].name.equals("Aged Brie")) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+
+                } else {
                     if (items[i].quality > 0) {
                         if (!items[i].name.equals("Canned Beans")) {
                             items[i].quality = items[i].quality - 1;
                         }
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
